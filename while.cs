2d81d9dd48cd47e83cfcfcd93350194a2b3847a9
iammpedri <pedroaugustope0@gmail.com>
2026-08-19 13:59:45 -0300

    prática de array e loop

--- while.cs
+++ while.cs
@@ -1,67 +1,67 @@
-// int contador = 1;
+int contador = 1;
 
-// while (contador <= 4)
-// {
-//     Console.WriteLine(contador);
-//     contador++;
-// }
+while (contador <= 4)
+{
+    Console.WriteLine(contador);
+    contador++;
+}
 
-// for (int contador = 1; contador <= 10; contador++)
-// {
-//     Console.WriteLine(contador);
-// }
+for (int contador = 1; contador <= 10; contador++)
+{
+    Console.WriteLine(contador);
+}
 
-// string[] Compras = {
-//     "Arroz",
-//     "Feijão",
-//     "Macarrão",
-//     "Carne",
-//     "Frango"
-// };
+string[] Compras = {
+    "Arroz",
+    "Feijão",
+    "Macarrão",
+    "Carne",
+    "Frango"
+};
 
-// foreach (string item in Compras)
-// {
-//     Console.WriteLine(item);
-// }
+foreach (string item in Compras)
+{
+    Console.WriteLine(item);
+}
 
-// string[] nomes = {
-//     "João",
-//     "Maria",
-//     "José",
-//     "Ana",
-//     "Pedro"
-// };
+string[] nomes = {
+    "João",
+    "Maria",
+    "José",
+    "Ana",
+    "Pedro"
+};
 
-// foreach (string nome in nomes)
-// {
-//     Console.WriteLine($"Olá, {nome}");
-// }
+foreach (string nome in nomes)
+{
+    Console.WriteLine($"Olá, {nome}");
+}
 
-// int[] numeros = { 1, 2, 4, 5, 6, 7, 8, 9, 10 };
-// foreach (int numero in numeros)
-// {
-//     if (numero % 2 == 0)
-//     {
-//         Console.WriteLine($"O número {numero} é par");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"O número {numero} é ímpar");
-//     }
-// }
+int[] numeros = { 1, 2, 4, 5, 6, 7, 8, 9, 10 };
+foreach (int numero in numeros)
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine($"O número {numero} é par");
+    }
+    else
+    {
+        Console.WriteLine($"O número {numero} é ímpar");
+    }
+}
 
-// int[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
-// foreach (int numero in numeros)
-// {
-//     if (numero % 3 ==0)
-//     {
-//         Console.WriteLine($"O número {numero} é divisível por 3");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"O número {numero} não é divisível por 3");
-//     }
-// }
+int[] numeros = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+foreach (int numero in numeros)
+{
+    if (numero % 3 ==0)
+    {
+        Console.WriteLine($"O número {numero} é divisível por 3");
+    }
+    else
+    {
+        Console.WriteLine($"O número {numero} não é divisível por 3");
+    }
+}
 
 double[] notas = { 7.5, 8.0, 6.5, 9.0, 5.5 };
 int aprovados = 0;
